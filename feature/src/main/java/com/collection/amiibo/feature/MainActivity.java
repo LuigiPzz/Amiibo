c1848a372697adbf6f753505e5b5c876dfc31996
Pozzi Luigi <luigi.pozzi@accenture.com>
2018-03-14 16:40:44 +0100

    Color & main page

--- feature/src/main/java/com/collection/amiibo/feature/MainActivity.java
+++ feature/src/main/java/com/collection/amiibo/feature/MainActivity.java
@@ -1,7 +1,10 @@
 package com.collection.amiibo.feature;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        Button bt_characters = findViewById(R.id.characters);
+        bt_characters.setOnClickListener(
+                        new View.OnClickListener(){
+                            @Override
+                            public void onClick(View v){
+                                Intent ac = new Intent(getApplicationContext(), CharactersActivity.class);
+                                startActivity(ac);
+                            }
+                        }
+        );
+
+        Button bt_cards = findViewById(R.id.cards);
+        //registra la callback dell'evento del click (codice eseguito al click)
+        bt_cards.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        Intent ac = new Intent(getApplicationContext(), CardsActivity.class);
+                        startActivity(ac);
+                    }
+                }
+        );
+ }
+
 }
